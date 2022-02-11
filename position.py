e2aa223a48ecccd57e2dcecd81814326f63ffe18
soheil bagheri <soheil.baghery@gmail.com>
2022-02-11 23:10:12 +0330

    Change order to position

--- position.py
+++ position.py
@@ -1,5 +1,5 @@
-class Order():
-    def __init__(self, id, pair, type, volume, openPrice, currentPrice, openAt, stopLoss = "", target = "") -> None:
+class Position():
+    def __init__(self, id, pair, type, volume, openPrice, currentPrice, openAt, stopLoss = "", takeProfit = "", comment="") -> None:
         self.id = id
         self.pair = pair
         self.type = type
@@ -8,15 +8,16 @@ class Order():
         self.currentPrice = currentPrice
         self.openAt = openAt
         self.stopLoss = stopLoss
-        self.target = target
+        self.takeProfit = takeProfit
         self.closeAt = ""
         self.profit = 0.0
         self.commission = 0.06
+        self.comment = comment
 
     def calcProfit(self):
         if self.closeAt != "":
             self.profit = self.currentPrice - self.openPrice
         return self.profit
 
-    def closeOrder(self, timestamp):
+    def closePosition(self, timestamp):
         self.closeAt = timestamp
---
+++ positionManager.py
@@ -0,0 +1,15 @@
+from position import Position
+import uuid
+
+class PositionManagert():
+    def __init__(self) -> None:
+        self.openPositions = []
+        self.closedPositions = []
+    
+    def openPosition(self, pair, type, price, volume, timestamp, stopLoss="", takeProfit="", comment=""):
+        positionId = uuid.uuid4()
+        newPosition = Position(positionId, pair, type, volume, price, price, timestamp, stopLoss, takeProfit, comment)
+        self.openPositions.append(newPosition)
+
+    def closePosition(self, timestamp):
+        self.openPositions[0].closePosition(timestamp)
