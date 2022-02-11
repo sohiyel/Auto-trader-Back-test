fef208bf695c51d1fde3a8b7686af69b2e9b033a
soheil bagheri <soheil.baghery@gmail.com>
2022-02-11 20:07:04 +0330

    Initialize Order manager class

---
+++ orderManager.py
@@ -0,0 +1,10 @@
+class OrderManagert():
+    def __init__(self) -> None:
+        self.openOrders = []
+        self.closedOrders = []
+    
+    def openOrder(pair, price, amount, timestamp, commission):
+        pass
+
+    def closeOrder(pair, price, amount, timestamp, commission):
+        pass
