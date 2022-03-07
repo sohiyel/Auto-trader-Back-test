cc459bd48995cc9e8fa38502d989e1caf05cbebe
soheil bagheri <soheil.baghery@gmail.com>
2022-03-07 18:50:12 +0330

    Read from and write into tasks json

--- tradeTask.py
+++ tradeTask.py
@@ -1,5 +1,5 @@
 import json
-
+import time
 class TradeTask():
     def __init__(self) -> None:
         self.pair = "BTC-USDT"
@@ -14,25 +14,45 @@ class TradeTask():
         self.optimization = False
         self.randomInputs = False
         self.numberOfInputs = 1
+        self.jsonFile = ""
 
     def read_toDo(self):
-        with open("task.json", "w") as json_data_file:
-            jsonFile = json.load(json_data_file)
-            if len(jsonFile["To_Do"]) > 0:
-                self.pair = jsonFile["To_Do"][0]["pair"]
-                self.timeFrame = jsonFile["To_Do"][0]["timeFrame"]
-                self.strategyName = jsonFile["To_Do"][0]["strategyName"]
-                self.botName = jsonFile["To_Do"][0]["botName"]
-                self.startAt = jsonFile["To_Do"][0]["startAt"]
-                self.endAt = jsonFile["To_Do"][0]["endAt"]
-                self.volume = jsonFile["To_Do"][0]["volume"]
-                self.initialCapital = jsonFile["To_Do"][0]["initialCapital"]
-                self.market = jsonFile["To_Do"][0]["market"]
-                self.optimization = jsonFile["To_Do"][0]["optimization"]
-                self.randomInputs = jsonFile["To_Do"][0]["randomInput"]
-                self.numberOfInputs = jsonFile["To_Do"][0]["numberOfInput"]
+        with open("tasks.json", "r") as json_data_file:
+            self.jsonFile = json.load(json_data_file)
+            if len(self.jsonFile["To_Do"]) > 0:
+                self.pair = self.jsonFile["To_Do"][0]["pair"]
+                self.timeFrame = self.jsonFile["To_Do"][0]["timeFrame"]
+                self.strategyName = self.jsonFile["To_Do"][0]["strategyName"]
+                self.botName = self.jsonFile["To_Do"][0]["botName"]
+                self.startAt = self.jsonFile["To_Do"][0]["startAt"]
+                self.endAt = self.jsonFile["To_Do"][0]["endAt"]
+                self.volume = self.jsonFile["To_Do"][0]["volume"]
+                self.initialCapital = self.jsonFile["To_Do"][0]["initialCapital"]
+                self.market = self.jsonFile["To_Do"][0]["market"]
+                self.optimization = self.jsonFile["To_Do"][0]["optimization"]
+                self.randomInputs = self.jsonFile["To_Do"][0]["randomInputs"]
+                self.numberOfInputs = self.jsonFile["To_Do"][0]["numberOfInputs"]
 
-                del jsonFile["To_Do"][0]
-                json.dump(jsonFile, json_data_file)
             else:
                 print("There is no tasks in the queue!")
+
+    def done_task(self):
+        task = {}
+        task["pair"] = self.pair
+        task["timeFrame"] = self.timeFrame
+        task["strategyName"] = self.strategyName
+        task["botName"] = self.botName
+        task["startAt"] = self.startAt
+        task["endAt"] = self.endAt
+        task["volume"] = self.volume
+        task["initialCapital"] = self.initialCapital
+        task["market"] = self.market
+        task["optimization"] = self.optimization
+        task["randomInputs"] = self.randomInputs
+        task["numberOfInputs"] = self.numberOfInputs
+        task["doneAt"] = time.strftime("%Y-%m-%d_%H-%M-%S")
+
+        with open("tasks.json", "w") as json_data_file:
+            del self.jsonFile["To_Do"][0]
+            self.jsonFile["Done"].append(task)
+            json.dump(self.jsonFile, json_data_file)
--- trade_runner.py
+++ trade_runner.py
@@ -7,67 +7,61 @@ from pprint import pprint
 import time
 from datetime import datetime
 import concurrent.futures
+from tradeTask import TradeTask
 
-pair =  "BTC-USDT"
-timeFrame = "4hour"
-strategyName = "RSIStrategy"
-botName = "Bot02"
-startAt = "2021-01-01 00:00:00"
-endAt = "2021-09-01 00:00:00"
-volume = 1
-initialCapital = 100000
-market = "spot"
-optimization = True
-randomInputs = False
-numberOfInputs = 5
+multiProcess = False
 
-
-
+tradeTask = TradeTask()
+tradeTask.read_toDo()
 
 def run_trader(currentInput):
     print  ("--------- New process started ---------")
-    trader = Trader(market = market,
-                    pair = pair,
-                    timeFrame = timeFrame,
-                    startAt = startAt,
-                    endAt = endAt,
-                    initialCapital = initialCapital,
-                    strategyName = strategyName,
-                    botName= botName,
-                    volume = volume,
+    trader = Trader(market = tradeTask.market,
+                    pair = tradeTask.pair,
+                    timeFrame = tradeTask.timeFrame,
+                    startAt = tradeTask.startAt,
+                    endAt = tradeTask.endAt,
+                    initialCapital = tradeTask.initialCapital,
+                    strategyName = tradeTask.strategyName,
+                    botName= tradeTask.botName,
+                    volume = tradeTask.volume,
                     currentInput = currentInput,
-                    optimization = optimization)
+                    optimization = tradeTask.optimization)
     result = trader.mainloop()
     print  ("--------- A process has finished! ---------")
     return result
 
 
 start_time = time.time()
-userInput = UserInput(pair, timeFrame, strategyName, botName, optimization, randomInputs)
+userInput = UserInput(tradeTask.pair, tradeTask.timeFrame, tradeTask.strategyName, tradeTask.botName, tradeTask.optimization, tradeTask.randomInputs)
 print("Number of steps: " + str(len(userInput.inputs)))
 
 if __name__ == '__main__':
     results = []
-    if not randomInputs:
+    if not tradeTask.randomInputs:
         numberOfInputs = len(userInput.inputs)
-
-    with concurrent.futures.ProcessPoolExecutor() as executer:
-        results = executer.map( run_trader, userInput.inputs)
+    if multiProcess:
+        with concurrent.futures.ProcessPoolExecutor() as executer:
+            results = executer.map( run_trader, userInput.inputs)
+    else:
+        for i in userInput.inputs:
+            results.append(run_trader(i))
         
 
-    if optimization:
+    if tradeTask.optimization:
         results = pd.concat(results)
         timestr = time.strftime("%Y-%m-%d_%H-%M-%S")
         results = results.sort_values(by = 'Net profit per day', ascending = False)
         optimumResult = results.iloc[0]
         # print(optimumResult["TwoEMA_slow_len"])
         print (results)
-        if botName:
-            path = "optimizations/" + timestr + "_" + pair + "_" + timeFrame + "_" + botName +".csv"
+        if tradeTask.botName:
+            path = "optimizations/" + timestr + "_" + tradeTask.pair + "_" + tradeTask.timeFrame + "_" + tradeTask.botName +".csv"
         else:
-            path = "optimizations/" + timestr + "_" + pair + "_" + timeFrame + "_" + strategyName +".csv"
+            path = "optimizations/" + timestr + "_" + tradeTask.pair + "_" + tradeTask.timeFrame + "_" + tradeTask.strategyName +".csv"
         results.to_csv(path)
         userInput.writeOptimizedValues(optimumResult)
             
         print("--- End of optimization: {endTime} ---".format(endTime=str(datetime.fromtimestamp(time.time()))))
         print("--- Duration: %s seconds ---" % (time.time() - start_time))
+        tradeTask.done_task()
