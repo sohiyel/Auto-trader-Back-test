413e9af5aedf9a1b33e9e62d5c5f0f3bc1fa5e10
soheil bagheri <soheil.baghery@gmail.com>
2022-05-10 14:10:40 +0430

    Fix bot signals

--- src/signalManager.py
+++ src/signalManager.py
@@ -8,7 +8,7 @@ class SignalManager():
         if botName == "":
             self.signal = SingleStrategy(strategyName, currentInput, pair, settings, marketData)
         else:
-            self.signal = BotSignal(botName, currentInput, pair, settings, marketData, marketData)
+            self.signal = BotSignal(botName = botName,currentInput = currentInput, pair = pair, settings = settings, marketData = marketData)
 
     def getSignal(self, marketData, timeStamp = ""):
         signal = self.signal.decider(marketData, timeStamp)
