724764e635ce6994080a17415f50d0a981a24520
soheil bagheri <soheil.baghery@gmail.com>
2022-05-24 13:24:58 +0430

    Change Okex fetch_balance

--- src/exchanges/okex.py
+++ src/exchanges/okex.py
@@ -5,25 +5,32 @@ from src.logManager import get_logger
 class Okex(BaseExchange):
     def __init__(self, settings, sandBox = False):
         self.settings = settings
-        super().__init__(sandBox)
         self.exchange = ccxt.okex()
         self.exchange.set_sandbox_mode(sandBox)
         self.logger = get_logger(__name__, settings)
+        self.authorize()
         
     def fetch_balance(self):
         try:
             response = self.exchange.fetch_balance()
+            self.logger.debug("Fetch balance response: ", response)
             if response['info']['code'] == '0':
                 return {
-                    'Equity' : response['info']['data']['accountEquity'],
-                    'Balance' : response['info']['data']['availableBalance']
+                    'Equity' : float(response['info']['data'][0]['details'][0]['availEq']),
+                    'Balance' : float(response['info']['data'][0]['details'][0]['cashBal'])
                 }
             else:
                 self.logger.error("Problem in getting account equity!")
                 self.logger.error (response)
                 return False
-        except:
-            self.logger.error("Cannot fetch balance from ccxt!")
+        except Exception as e:
+            self.logger.error("Cannot fetch balance from ccxt!"+ str(e))
             return False
+
+    def create_market_order(self, symbol, side, amount, price=None, params={}):
+        nparams = {
+            'tdMode': 'isolated'
+        }
+        return self.exchange.create_market_order(symbol, side, amount, nparams)
 
 
