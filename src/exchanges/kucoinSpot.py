029517404cbea8d51ccde08e82cd6657a85861e9
soheil bagheri <soheil.baghery@gmail.com>
2022-06-05 21:19:13 +0430

    Add kucoin spot trading feature

--- src/exchanges/kucoinSpot.py
+++ src/exchanges/kucoinSpot.py
@@ -1,72 +1,55 @@
-from src.exchanges.kucoin import Kucoin
-import requests
-import asyncio
-import configparser
+from src.exchanges.baseExchange import BaseExchange
 import ccxt as ccxt
-from datetime import datetime
-from time import sleep
-from src.utility import Utility
 from src.logManager import LogService
-class KucoinSpot(Kucoin):
+class KucoinSpot(BaseExchange):
     def __init__(self, settings, sandBox = False):
-        super().__init__(sandBox)
         self.settings = settings
         self.baseUrl = 'https://api.kucoin.com'
         self.exchange = ccxt.kucoin()
         self.exchange.set_sandbox_mode(sandBox)
         self.logService = LogService(__name__, settings)
-        self.logger = self.logService.logger  #get_logger(__name__, settings)
+        self.logger = self.logService.logger
 
-    def authorize(self):
-        cfg = configparser.ConfigParser()
+    def change_symbol_for_trade(self, symbol):
+        if "/" in symbol:
+            if ":" in symbol:
+                symbols = symbol.split(":")
+                return symbols[0].upper()
+            else:
+                return symbol.upper()
+        elif "_" in symbol:
+            symbols = symbol.split("_")
+            return symbols[0].upper() + "/" + symbols[1].upper()
+        elif "-" in symbol:
+            symbols = symbol.split("-")
+            return symbols[0].upper() + "/" + symbols[1].upper()
+
+    def change_symbol_for_data(self, symbol):
+        return self.change_symbol_for_trade(symbol)
+
+    def change_symbol_for_markets(self, symbol):
+        return self.change_symbol_for_data(symbol)
+
+    def fetch_balance(self):
+        response = self.exchange.fetch_accounts()
+        for i in response:
+            if i['type'] == 'trade':
+                return {'Balance': i['info']['balance'],
+                        'Equity': i['info']['available']}
+        return
+
+    def get_contract_size(self, markets, pair):
         try:
-            if self.sandBox:
-                cfg.read(self.settings.API_SANDBOX_PATH)
+            ePair = self.change_symbol_for_markets(pair) #Utility.get_exchange_format(pair+":USDT")
+            for i in markets:
+                if ePair in i:
+                    ePair = i
+                    break
+            marketData = markets[ePair]
+            if marketData['info']['baseMinSize']:
+                return marketData['info']['baseMinSize']
             else:
-                cfg.read(self.settings.API_PATH)
-            self.exchange.apiKey = cfg.get('KEYS','api_key')
-            self.exchange.secret = cfg.get('KEYS', 'api_secret')
-            self.exchange.password = cfg.get('KEYS', 'api_passphrase')
-        except:
-            self.logger.error("Cannot read exchange config file!")
-
-    def get_klines(self, symbol, timeFrame, startAt, endAt):
-        pts = {'pair': symbol, 'timeFrame': timeFrame, 'strategyName': 'NaN'}
-        self.logService.set_pts_formatter(pts)
-        kLineURL = '/api/v1/market/candles?'
-        timeFrame = Utility.unify_timeframe(timeFrame, "kucoin")
-        params = {
-            'type': timeFrame,
-            'symbol': symbol,
-            'startAt': startAt,
-            'endAt': endAt
-        }
-        self.logger.info('requesting data for {} in timeframe {} from {} to {}'.format(symbol,timeFrame, str(datetime.fromtimestamp(startAt)), str(datetime.fromtimestamp(endAt))))
-        status = True
-        while status:
-            response = requests.get(self.baseUrl+kLineURL,params=params)
-            if response.status_code != 200:
-                self.logger.error('Something went wrong. Error: {} sleeping ... '.format(response.status_code))
-                sleep(10)
-            else:
-                
-                self.logger.info('Success! recieved {} candles'.format(len(response.json()['data'])))
-                return(response.json()['data'])
-
-    async def get_klines_data(self, symbol, timeFrame, startAt, endAt, limit):
-        klines = []
-        timeFrame = Utility.unify_timeframe(timeFrame, "kucoin")
-        for i in range(startAt,endAt,limit):
-            temp = []
-            if (i+limit < endAt):
-                temp.extend(self.get_klines(symbol, timeFrame, i, i+limit))
-            else:
-                temp.extend(self.get_klines(symbol, timeFrame, i, endAt))
-            if temp:
-                if len(temp) > 0:
-                    klines.extend(temp)
-                    await asyncio.sleep(5)
-            else:
-                break
-
-        return klines
+                self.logger.error(f"Cannot find contractSize of {ePair}!")
+                raise ValueError(f'Cannot find contractSize of {ePair}!')
+        except Exception as e:
+            self.logger.error(f"Cannot get contract size of {ePair}" + str(e))
